7ea448a9eee5988289962e511b5723e264f14272
andrevz <andrevz11@icloud.com>
2022-10-07 22:56:08 -0400

    Create Map Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,54 @@
 import NavBar from './NavBar';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import { useState } from 'react';
+import Map from './Map';
 
 function Home() {
+  const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState('');
+
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleSignOut(e) {
+    e.preventDefault();
+
+    try {
+      await logout();
+      navigate('/login', { replace: true });
+    } catch (error) {}
+  }
+
+  function filterUsers() {
+    let filtered = [...users];
+    if (!query) {
+      return filtered;
+    }
+
+    if (+query > 0) {
+      filtered = filtered.filter(u => u.bikeNumber.toString().startsWith(query));
+    }
+
+    if (query === 'ocu') {
+      filtered = filtered.filter(u => u.status === 1 && u.onFrequency);
+    } else if (query === 'lib') {
+      filtered = filtered.filter(u => u.status === 0 && u.onFrequency);
+    } else if (query.length >= 3) {
+      filtered = filtered.filter(u => u.fullName.toLowerCase().includes(query));
+    }
+
+    return filtered;
+  }
+
+  const filteredUsers = filterUsers();
+
   return (
     <div className='h-screen w-screen'>
-      <NavBar />
+      <NavBar handleSignOut={handleSignOut} />
+      <section className='flex fixed top-auto h-full w-full'>
+        <Map users={filteredUsers} />
+      </section>
     </div>
   );
 }
---
+++ src/components/Map.jsx
@@ -0,0 +1,76 @@
+import 'leaflet/dist/leaflet.css';
+
+import * as L from 'leaflet/dist/leaflet';
+import { useEffect, useState } from 'react';
+
+function Map({ users }) {
+  const [mapLayer, setMapLayer] = useState();
+  const [markerLayers, setMarkerLayers] = useState([]);
+
+  useEffect(() => {
+    const map = L.map('map').setView([-17.370791, -66.18359], 14);
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap',
+    }).addTo(map);
+
+    setMapLayer(map);
+
+    return () => {
+      const container = L.DomUtil.get("map");
+      if (container) {
+        container._leaflet_id = null;
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    removeUserMarkers();
+    createUserMarkers(); 
+  }, [users]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  function createUserMarkers() {
+    if (!mapLayer) {
+      return;
+    }
+
+    const markers = [];
+    for (const user of users) {
+      if (!user.lat || !user.long) {
+        continue;
+      }
+
+      const circle = L.circle([ user.lat, user.long ], {
+        color: !user.onFrequency ? 'gray' : (user.status === 1 ? 'red' : 'green'),
+        fillColor: 'white',
+        fillOpacity: 0.5,
+        radius: 70
+      }).addTo(mapLayer);
+
+      circle.bindTooltip(`${user.bikeNumber}`, {
+        permanent: true,
+        direction: 'top',
+        className: 'small-tooltip'
+      }).openTooltip();
+
+      markers.push(circle);
+    }
+    setMarkerLayers(markers);
+  }
+
+  function removeUserMarkers() {
+    if (!mapLayer || !markerLayers) {
+      return;
+    }
+
+    markerLayers.forEach(marker => mapLayer.removeLayer(marker));
+    setMarkerLayers([]);
+  }
+
+  return (
+    <div id='map' className='w-full h-full z-10'></div>
+  );
+}
+
+export default Map;
