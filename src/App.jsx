86b272577a8201bd676dfeb982024f3785f237b6
andrevz <andrevz11@icloud.com>
2022-10-07 20:56:20 -0400

    Add Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-    </div>
-  )
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App
