a5aba7a65d8b00e3217b861e4555908e43e18fd9
andrevz <andrevz11@icloud.com>
2022-10-07 23:40:14 -0400

    Add Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './navigation/ProtectedRoute';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
@@ -16,6 +19,14 @@ function App() {
         } />
         <Route path='/login' element={<Login />} />
       </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+      />
     </AuthProvider>
   );
 }
